199c9fd61e40357036dd0526a507eb54d11681bc
vonor56 <113927003+vonor56@users.noreply.github.com>
2022-11-14 20:57:33 +0200

    te

---
+++ Ovlad/Session 16/index.js
@@ -0,0 +1,10 @@
+// aceasta linie este comentata
+
+/**
+ * multi line comment
+ * nu o sa fie luat in considerare
+*/
+
+// primitives (primitive) - number, string, boolean, undefined, symbol
+// References (referinte) - objects: array, functions, null
+
