95ef43b35cd09cbec9453c7c905fa684d0baf8dd
unknown <gnegru945@gmail.com>
2022-11-16 21:05:45 +0200

    social media

---
+++ ngabriel/index.js
@@ -0,0 +1,36 @@
+// Aceasta este o linie de cod comentat si o fac cu ctrl+/
+
+/**
+ * Multi line comment
+ * care nu va fi luat in considerare
+ */
+
+// Primitives (primitive) - number, string, boolean, undefined, symbol
+// References (referinte) - objects: array, functions, null
+
+/**
+ * Variables types
+ */
+
+// var - nu se mai foloseste
+// let - variabile dinamica, i se poate schimba valoarea
+// const - variabila statica, nu i se poate schimba valoarea
+
+
+/**
+ * Numbers
+ */
+
+let monthlyPaycheck = 3000;
+console.log(monthlyPaycheck);
+monthlyPaycheck = 5000;
+console.log(monthlyPaycheck);
+
+let aNumber = 11;
+let bNumber = 5;
+let total;
+
+total = aNumber + bNumber;
+
+
+console.log(total)
