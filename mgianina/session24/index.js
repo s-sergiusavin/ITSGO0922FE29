c488f873dcfa52e0476d9b7d1eed0efff0f077ea
GianinaMM <morosan.gianina1@gmail.com>
2022-12-14 21:01:24 +0200

    Session 24

---
+++ mgianina/session24/index.js
@@ -0,0 +1,131 @@
+const demoBoxElement = document.getElementById("testBox");
+
+console.dir(demoBoxElement);
+
+demoBoxElement.className = "red";
+demoBoxElement.className = " ";
+
+demoBoxElement.classList.add("blue"); //ca sa adaugam o clasa
+
+demoBoxElement.classList.add("red");
+
+demoBoxElement.classList.remove("blue"); //ca sa stergem o clasa folosim remove()
+
+demoBoxElement.classList.toggle("blue"); //daca nu are toggle ,o adauga,daca o are ,atunci o sterge
+
+//get or set attributes
+
+const demoImageElement = document.getElementById("demoImage");
+const newImageElement = document.getElementById("newImage");
+
+const imgSrc = demoImageElement.getAttribute("src");
+
+newImageElement.setAttribute("src", imgSrc);
+newImageElement.setAttribute(
+  "src",
+  "https://cdn.pixabay.com/photo/2018/08/14/13/23/ocean-3605547__340.jpg"
+);
+
+/**
+ * Event listeners //in js dispare on
+ */
+
+const ulListElement = document.getElementsByTagName("ul")[0];
+
+let listItemsElements = document.querySelectorAll("li");
+
+// ulListElement.addEventListener("click", () => {
+//     ulListElement.classList.toggle("blue");
+// });
+
+for (let i = 0; i < listItemsElements.length; i++) {
+  const listElement = listItemsElements[i];
+  // listElement.addEventListener("mouseover", function () {
+  //     this.classList.add("red");
+  //     console.log(this);
+  // });
+  // listElement.addEventListener("mouseout", function () {
+  //     this.classList.remove("red");
+  // });
+  listElement.addEventListener("click", toggleListItems);
+}
+
+//mouseouver este event Listener si apoi punem functie sa actioneze
+
+function toggleListItems() {
+  const toggleValue = this.classList.toggle("red");
+  console.log(toggleValue);
+  if (toggleValue) {
+    const span = document.createElement("span");
+    span.innerHTML = "x";
+    this.appendChild(span);
+  } else {
+    this.getElementsByTagName("span")[0].remove();
+    this.remove();
+  }
+}
+
+const clickMeElement = document.getElementById("clickMe");
+clickMeElement.style.cursor = "pointer";
+
+const demoInputElement = document.getElementById("demoInput");
+
+demoInputElement.addEventListener("keypress", function (event) {
+  console.log(event);
+  if (event.keyCode === 13 && demoInputElement.value.length > 3) {
+    demoInputElement.style.background = "green";
+  }
+});
+
+clickMeElement.addEventListener("click", function () {
+  const newLiElement = document.createElement("li");
+  ulListElement.appendChild(newLiElement);
+  listItemsElements = document.querySelectorAll("li");
+  if (demoImageElement.value) {
+    newLiElement.innerHTML = demoInputElement.value;
+    demoInputElement.value = "";
+  } else {
+    newLiElement.innerHTML = "Elem " + listItemsElements.length;
+  }
+});
+
+const toDoInputElement = document.getElementById("toDoInput");
+const addButtonElement = document.getElementsByClassName("addButton")[0];
+const toDoListElement = document.getElementsByClassName("toDoItems")[0];
+
+addButtonElement.addEventListener("click", function () {
+  const canAdd = checkPlan();
+
+  if (canAdd) {
+    if (toDoInputElement.value !== "") {
+      const newListItemElem = document.createElement("li");
+      newListItemElem.className = "newListItems";
+
+      newListItemElem.addEventListener("dblclick", function () {
+        this.remove()
+      });
+
+      toDoListElement.appendChild(newListItemElem);
+      newListItemElem.innerHTML = toDoInputElement.value;
+      toDoInputElement.value = "";
+    }else {
+        alert("Te rugam sa introduci o valoare!!"); 
+    }
+  } else {
+    alert("Te rugam sa treci un plan tarifar superior!!"); //dupa alerta tot codul nu se mai executa pana nu dam enter
+    // prompt("Te rugam sa treci un plan tarifar superior!!", "Iti permiti?")
+    // confirm("Te rugam sa treci un plan tarifar superior!!"); //ne permite ca utilizatorul sa returneze rezultatul
+    console.log("Actiune dupa alert");
+  }
+});
+
+function checkPlan() {
+  const newListItemsNumber =
+    document.getElementsByClassName("newListItems").length;
+  return newListItemsNumber < 3;
+}
+
+
+
+
+
