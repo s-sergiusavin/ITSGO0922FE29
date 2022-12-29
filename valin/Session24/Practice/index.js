13770b3ecf74d31e2a6dffa5a2c62c23292a7247
alinvs94 <alin.vasiliu94@gmail.com>
2022-12-29 17:44:12 +0100

    Practice

--- valin/Session24/Practice/index.js
+++ valin/Session24/Practice/index.js
@@ -1,15 +1,168 @@
-const num1 = document.getElementById('n1').innerHTML;
-const num2 = document.getElementById('n2').innerHTML;
-const num3 = document.getElementById('n3').innerHTML;
-const num4 = document.getElementById('n4').innerHTML;
-const num5 = document.getElementById('n5').innerHTML;
-const num6 = document.getElementById('n6').innerHTML;
-const num7 = document.getElementById('n7').innerHTML;
-const num8 = document.getElementById('n8').innerHTML;
-const num9 = document.getElementById('n9').innerHTML;
+const num1Element = document.getElementById('n1');
+const num2Element = document.getElementById('n2');
+const num3Element = document.getElementById('n3');
+const num4Element = document.getElementById('n4');
+const num5Element = document.getElementById('n5');
+const num6Element = document.getElementById('n6');
+const num7Element = document.getElementById('n7');
+const num8Element = document.getElementById('n8');
+const num9Element= document.getElementById('n9');
+const num0Element= document.getElementById('n0');
+const dotElement= document.getElementById('dotBtn');
+
+const clearElement = document.getElementById('clearBtn');
+const percentageElement = document.getElementById('percentBtn');
+const powerElement = document.getElementById('powerBtn');
+const divideElement = document.getElementById('divideBtn');
+const multiplierElement = document.getElementById('multiplierBtn');
+const subtractionElement = document.getElementById('subtractionBtn');
+const addictionElement = document.getElementById('addictionBtn');
+const equalElement = document.getElementById('equalBtn');
+const displayElement = document.querySelector('.calcDisplay');
+const displayContainerElement = document.querySelector('.calcDisplayContainer');
 
 
 
-function showNumber() {
+let accumulator = 0;
+let total = 0;
+let operation = '';
 
-}
+// if (displayElement.innerHTML.length > 18) {
+//    displayElement.style.fontSize = '20px';
+// }
+
+
+   num1Element.addEventListener('click', function() {
+      const num1 = num1Element.innerHTML;
+      deleteZeros();
+      displayElement.innerHTML = displayElement.innerHTML + num1;
+   })
+
+   num2Element.addEventListener('click', function() {
+      const num2 = num2Element.innerHTML;
+      deleteZeros();
+      displayElement.innerHTML = displayElement.innerHTML + num2;
+   })
+
+   num3Element.addEventListener('click', function() {
+      const num3 = num3Element.innerHTML;
+      deleteZeros();
+      displayElement.innerHTML = displayElement.innerHTML + num3;
+   })
+
+   num4Element.addEventListener('click', function() {
+      const num4 = num4Element.innerHTML;
+      deleteZeros();
+      displayElement.innerHTML = displayElement.innerHTML + num4;
+   })
+
+   num5Element.addEventListener('click', function() {
+      const num5 = num5Element.innerHTML;
+      deleteZeros();
+      displayElement.innerHTML = displayElement.innerHTML + num5;
+   })
+
+   num6Element.addEventListener('click', function() {
+      const num6 = num6Element.innerHTML;
+      deleteZeros();
+      displayElement.innerHTML = displayElement.innerHTML + num6;
+   })
+
+   num7Element.addEventListener('click', function() {
+      const num7 = num7Element.innerHTML;
+      deleteZeros();
+      displayElement.innerHTML = displayElement.innerHTML + num7;
+   })
+
+   num8Element.addEventListener('click', function() {
+      const num8 = num8Element.innerHTML;
+      deleteZeros();
+      displayElement.innerHTML = displayElement.innerHTML + num8;
+   })
+
+   num9Element.addEventListener('click', function() {
+      const num9 = num9Element.innerHTML;
+      deleteZeros();
+      displayElement.innerHTML = displayElement.innerHTML + num9;
+   })
+
+   num0Element.addEventListener('click', function() {
+      const num0 = num0Element.innerHTML;
+      deleteZeros();
+      displayElement.innerHTML = displayElement.innerHTML + num0;
+   })
+
+   dotElement.addEventListener('click', function() {
+      const dot = dotElement.innerHTML;
+      displayElement.innerHTML = displayElement.innerHTML + dot;
+   })
+
+   clearElement.addEventListener('click', function() {
+      displayElement.innerHTML = '0';
+      total = 0;
+      accumulator = 0;
+      operation = 0;
+   })
+
+   percentageElement.addEventListener('click', function() {
+      operation = 1;
+      accumulatorValue();
+   })
+
+   powerElement.addEventListener('click', function() {
+         total = displayElement.innerHTML * displayElement.innerHTML;
+         displayElement.innerHTML = total;
+    
+   })
+
+   divideElement.addEventListener('click', function() {
+      operation = 2;
+      accumulatorValue();
+   })
+
+   multiplierElement.addEventListener('click', function() {
+      operation = 3;
+      accumulatorValue();
+   })
+
+   subtractionElement.addEventListener('click', function() {
+      operation = 4;
+      accumulatorValue();
+   })
+
+   addictionElement.addEventListener('click', function() {
+      operation = 5;
+      accumulatorValue();
+   })
+
+   equalElement.addEventListener('click', function() {
+      if (operation === 1) {
+         total = (accumulator * displayElement.innerHTML) / 100;
+      } else if (operation === 2) {
+         total = accumulator / displayElement.innerHTML;
+      } else if (operation === 3) {
+         total = accumulator * displayElement.innerHTML;
+      } else if (operation === 4) {
+         total = accumulator - displayElement.innerHTML;
+      } else if (operation === 5) {
+         total = Number(accumulator) + Number(displayElement.innerHTML);
+      }
+
+      displayElement.innerHTML = total;
+      operation = 0;
+      accumulator = 0;
+   })
+
+   function deleteZeros() {
+      if (displayElement.innerHTML[0] === '0' && displayElement.innerHTML[1] !== '.') {
+         displayElement.innerHTML = '';
+      }
+   }
+
+   function accumulatorValue() {
+      total = 0;
+      if (displayElement.innerHTML !== '0') {
+         accumulator = displayElement.innerHTML;
+         displayElement.innerHTML = '0';
+      }
+   }
--- valin/Session25/index.js
+++ valin/Session25/index.js
@@ -6,7 +6,7 @@
 let string = "abcdef";
 let pattern = /ab/;
 
-console.log(pattern.exec(string)); // practic cauta pereche ab in stringul nostru si ne returneaza un obiect cu ceva informatii
+console.log(pattern.exec(string)); // practic cauta pereche ab in stringul nostru si ne returneaza un array cu ceva informatii
 console.log(pattern.test(string)); // returneaza true daca exista
 console.log(string.match(pattern)); // returneaza acelasi lucru ca exec doar scris in mod divers
 
@@ -50,13 +50,13 @@ const validateName = (value, regex) => {
 }
 
 const validateAge = (value) => {
-   return !isNaN(value); 
+   return !isNaN(value);
 }
 
 const test = "test";
 console.log(test);
 console.log(!test);
-console.log(!!test); // imi forteaza valoarea booleana al expresiei
+console.log(!!test); // imi forteaza valoarea booleana al expresiei (0, null, undefined = false in booleana)
 
 const clearSigninInput = () => {
   emailField.value = "";
@@ -68,86 +68,60 @@ const clearSigninInput = () => {
 };
 
 submitBtn.addEventListener("click", function (event) {
-  event.preventDefault();
+  event.preventDefault(); // cand dam click pe un buton de tipul submit dupa da referesh la pagina, si cu metoda asta
+  // ii oprim operatiile default dupa ce dam click
 
   const emailValue = emailField.value;
   const passwordValue = passwordField.value;
   const firstNameValue = firstNameField.value;
   const lastNameValue = lastNameField.value;
   const ageValue = ageField.value;
-  const regexEmailPattern = /\D{4,}\@\D{4,}\.\D{2,}/g;
+  const regexEmailPattern = /\D{4,}\@\D{4,}\.\D{2,}/g; // nu punem backend-ul sa lucreze degeaba sa controleze daca emailul e corect
   const regexNamePattern = /^[a-zA-ZàáâäãåąčćęèéêëėįìíîïłńòóôöõøùúûüųūÿýżźñçčšžÀÁÂÄÃÅĄĆČĖĘÈÉÊËÌÍÎÏĮŁŃÒÓÔÖÕØÙÚÛÜŲŪŸÝŻŹÑßÇŒÆČŠŽ∂ð ,.'-]+$/u;
 
-  if (emailValue === "" || passwordValue === "" || firstNameField.value === '' || lastNameField.value === '' || ageField.value === '') {
-    showError("All fields are required and must contain a value");
-  } else {
-    error.style.display = "none";
+  if (logInToggle === false) {
 
-    if (
-      validatePassword(passwordValue) && validateEmail(emailValue, regexEmailPattern)) {
+    if (emailValue === "" || passwordValue === "" || firstNameField.value === '' || lastNameField.value === '' || ageField.value === '') {
+      showError("All fields are required and must contain a value");
+    } else {
+      error.style.display = "none";
 
-      if (validateName(firstNameValue, regexNamePattern)) {
+      if (validatePassword(passwordValue) && validateEmail(emailValue, regexEmailPattern)) {
 
-        if (validateName(lastNameValue, regexNamePattern)) {
+        if (validateName(firstNameValue, regexNamePattern)) {
 
-          if (validateAge(ageValue)) {
-            alert("Signed in!");
-            clearSigninInput();
+          if (validateName(lastNameValue, regexNamePattern)) {
+
+            if (validateAge(ageValue)) {
+              alert("Signed in!");
+              clearSigninInput();
+            } else {
+              alert("Try again");
+              showError("Invalid Age");
+              clearSigninInput();
+            }
           } else {
             alert("Try again");
-            showError("Invalid Age");
+            showError("Invalid Last Name");
             clearSigninInput();
           }
         } else {
           alert("Try again");
-          showError("Invalid Last Name");
+          showError("Invalid First Name");
           clearSigninInput();
         }
       } else {
         alert("Try again");
-        showError("Invalid First Name");
+        showError("Incorrect email or passowrd");
         clearSigninInput();
       }
-    } else {
-      alert("Try again");
-      showError("Incorrect email or passowrd");
-      clearSigninInput();
     }
-  }
-
-//   console.dir(emailField);
-});
-
-const switchBtnElementSign = document.querySelector('#switchToLog');
-
-switchBtnElementSign.addEventListener('click', function() {
-  document.getElementById("switchToLog").id= 'switchToSign';
-  document.getElementById('switchToSign').value = 'Switch to Sign In';
-
-  firstNameField.style.display = 'none';
-  lastNameField.style.display = 'none';
-  ageField.style.display = 'none';
-  const labelElements = document.querySelectorAll("label");
-  for (let i = 0; i < labelElements.length; i++) {
-    labelElements[i].style.display = 'none';
-  }
-  document.querySelector('h2').innerHTML = 'Log In';
-  const subBtn = document.getElementById('signin');
-  subBtn.type = 'button';
-  subBtn.value = 'Log In';
-  subBtn.id = 'login';
-
-  const logBtn = document.getElementById('login');
-
-  logBtn.addEventListener('click', function() {
-  
-    const emailValue = emailField.value;
-    const passwordValue = passwordField.value;
-    const regexEmailPattern = /\D{4,}\@\D{4,}\.\D{2,}/g;
+  } else {
     if (emailValue === "" || passwordValue === "") {
-      showError("All fields are required and must contain a value");
+      showError('All fields are required and must contain a value')
     } else {
-      error.style.display = "none";
+      error.style.display = 'none';
+
       if (validatePassword(passwordValue) && validateEmail(emailValue, regexEmailPattern)) {
         alert("Signed in!");
         clearSigninInput();
@@ -156,18 +130,97 @@ switchBtnElementSign.addEventListener('click', function() {
         showError("Incorrect email or passowrd");
         clearSigninInput();
       }
+
     }
+
+  }
+//   console.dir(emailField);
+});
+const headerCredentialsElement = document.getElementsByTagName('h2');
+const switchBtnElementSign = document.querySelector('#switchToLog');
+const signUpLabelsElements = document.querySelectorAll(".labelContainer");
+const forgottenCredentialsElements = document.getElementsByClassName('forgottenCredentials')
+let logInToggle = false;
+
+const changeTitle = (title) => {
+  headerCredentialsElement[0].innerHTML = title;
+}
+
+const changeSwitchBtn = (value) => {
+  switchBtnElementSign.value = value
+}
+
+switchBtnElementSign.addEventListener('click', function() {
+  console.log(forgottenCredentialsElements)
+  logInToggle = !logInToggle;
   
-  })
+  if (logInToggle) {
+    forgottenCredentialsElements[0].style.display = '';
+    for (let i = 0; i < signUpLabelsElements.length; i++) {
+      signUpLabelsElements[i].style.display = 'none';
+    }
+    changeSwitchBtn('Switch to Sign Up');
+    changeTitle("Welcome back! Log In!");
+  } else {
+    forgottenCredentialsElements[0].style.display = 'none';
+    for (let i = 0; i < signUpLabelsElements.length; i++) {
+      signUpLabelsElements[i].style.display = '';
+      }
+    changeSwitchBtn('Switch to Log In');
+    changeTitle("Welcome, Sing Up!");
 
+  }
 
 })
 
-const switchBtnElementLog = document.getElementById('switchToSign');
+// switchBtnElementSign.addEventListener('click', function() {
+//   document.getElementById("switchToLog").id= 'switchToSign';
+//   document.getElementById('switchToSign').value = 'Switch to Sign In';
 
-switchBtnElementLog.addEventListener ('click', function() {
-  switchBtnElementLog.id = 'switchToLog';
-})
+//   firstNameField.style.display = 'none';
+//   lastNameField.style.display = 'none';
+//   ageField.style.display = 'none';
+//   const labelElements = document.querySelectorAll("label");
+//   for (let i = 0; i < labelElements.length; i++) {
+//     labelElements[i].style.display = 'none';
+//   }
+//   document.querySelector('h2').innerHTML = 'Log In';
+//   const subBtn = document.getElementById('signin');
+//   subBtn.type = 'button';
+//   subBtn.value = 'Log In';
+//   subBtn.id = 'login';
+
+//   const logBtn = document.getElementById('login');
+
+//   logBtn.addEventListener('click', function() {
+
+//     const emailValue = emailField.value;
+//     const passwordValue = passwordField.value;
+//     const regexEmailPattern = /\D{4,}\@\D{4,}\.\D{2,}/g;
+//     if (emailValue === "" || passwordValue === "") {
+//       showError("All fields are required and must contain a value");
+//     } else {
+//       error.style.display = "none";
+//       if (validatePassword(passwordValue) && validateEmail(emailValue, regexEmailPattern)) {
+//         alert("Signed in!");
+//         clearSigninInput();
+//       } else {
+//         alert("Try again");
+//         showError("Incorrect email or passowrd");
+//         clearSigninInput();
+//       }
+//     }
+
+//   })
+
+
+// })
+
+// const switchBtnElementLog = document.getElementById('switchToSign');
+
+// switchBtnElementLog.addEventListener ('click', function() {
+//   switchBtnElementLog.id = 'switchToLog';
+// })
 
 
 
@@ -188,7 +241,7 @@ switchBtnElementLog.addEventListener ('click', function() {
 /**
  * setTimeout
  * executa o functie pe care o primeste ca parametru o singura data dupa o anumita perioada de timp
- * timpul este exprimat in milisecunde upa ce s-a executat pagina
+ * timpul este exprimat in milisecunde dupa ce s-a executat pagina
  * daca perioada de timp nu este specificata, defaultul este 0 secunde dupa ce s-a executat tot codul din pagina
  */
 
@@ -227,8 +280,8 @@ const anotherFunction = myFunction();
 //    start += 1;
 //    if (start === 7) {
 //       setFields();
-//       clearInterval(interval);
+//       clearInterval(interval); // dam valoarea referintei care vrem sa oprim
 //    }
 // }, 2000);
 
-// // 
+// //
